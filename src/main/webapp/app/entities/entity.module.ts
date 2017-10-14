2cca8c1e53481cabb5298b4d86d4e40e96e2bdd0
Daniyal Ahmed Khan <daniyal.khan@getflashcall.com>
2017-10-15 04:18:12 +0500

    JHipster Project - Entities Generated

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterhelloworldBlogModule } from './blog/blog.module';
+import { JhipsterhelloworldEntryModule } from './entry/entry.module';
+import { JhipsterhelloworldTagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        JhipsterhelloworldBlogModule,
+        JhipsterhelloworldEntryModule,
+        JhipsterhelloworldTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
